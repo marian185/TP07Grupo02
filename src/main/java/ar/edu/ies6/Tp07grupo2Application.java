49ad4e296d84dc79d87714f5f37d64aa7c0af115
MI-PC <MI-PC@DESKTOP-OEM8JLM>
2024-10-16 22:07:58 -0300

    Inicializaciòn del proyecto Guerrero Mariana

---
+++ src/main/java/ar/edu/ies6/Tp07grupo2Application.java
@@ -0,0 +1,13 @@
+package ar.edu.ies6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp07grupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp07grupo2Application.class, args);
+	}
+
+}
